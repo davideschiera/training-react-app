b0bd2d1740a45f4b48f954ccc5a2b9a35a830b78
Davide Schiera <davide@sysdig.com>
2016-05-26 00:17:45 -0700

    Static content.

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,15 @@ ReactDOM.render(
     <div>
         <h1>Welcome!</h1>
         <h2>Dashboards</h2>
-        <ol>
-            <li><a>1. Something</a></li>
-            <li><a>2. Else</a></li>
-        </ol>
+        <ul>
+            <li><a>1 - Something</a></li>
+            <li><a>2 - Else</a></li>
+        </ul>
+        <h3>1 - Something</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
+        </p>
+        <strong>Loaded!</strong>
+        <span>1, 2, 3</span>
     </div>,
     document.getElementById('app')
 );
